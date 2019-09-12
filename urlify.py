fb4935aee2817932b2b0f347247d9379abd9abfe
isabela-dominguez <isabela.dohu@yahoo.com.mx>
2019-09-12 18:58:54 -0400

    v1

--- urlify.py
+++ urlify.py
@@ -1,12 +1,21 @@
 def urlify(s, l):
-    n =""
+    new_length = l
+
     for letter in s:
         if(letter == " "):
+            new_lenght += 1
+
+    for i in reversed(range(s)):
+        if(letter == " "): 
+            #start backwards becuase extra space is given
+            new_length += 1
             n += "%20"
         else: 
             n += letter 
 
     return n 
 
+    
 
-print(urlify("hi mi ", 4))
+
+print(urlify("this is my banana    ", 4))
